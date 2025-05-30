b4b594500f59248199cc426b5f27d73c9c3ff0c8
Henrique Klayton <henrique-klayton@users.noreply.github.com>
2025-05-29 22:00:56 -0300

    WIP Websocket no python utilizando socket.io

--- src/lumi-bot/websocket/main.py
+++ src/lumi-bot/websocket/main.py
@@ -1,19 +1,53 @@
-import asyncio
+import socketio  # type: ignore[import-untyped]
+from aiohttp import web
 
-from websockets.asyncio.server import serve
+sio = socketio.AsyncServer(
+    async_mode="aiohttp",
+    cors_allowed_origins=[
+        "http://localhost:*",
+        "https://admin.socket.io",
+        "https://piehost.com",
+    ],
+)
+sio.instrument(auth={"username": "admin", "password": "admin123lumi"})
+clients: set[str] = set()
 
 
-async def handler(websocket):
-    async for message in websocket:
-        await websocket.send(message)
+@sio.event
+async def connect(sid, environ, auth=None):
+    print("connect ", sid)
+    clients.add(sid)
+    print(f"current clients: {', '.join(clients)}")
 
 
-async def main():
-    print("Starting handler server on ws://localhost:8765")
-    async with serve(handler, "localhost", 8765) as server:
-        await server.serve_forever()
+@sio.event
+async def msg(sid, text):
+    print(f"received: {text}")
 
+
+@sio.event
+async def disconnect(sid, reason):
+    if reason == sio.reason.CLIENT_DISCONNECT:
+        print("the client disconnected")
+    elif reason == sio.reason.SERVER_DISCONNECT:
+        print("the server disconnected the client")
+    else:
+        print("disconnect reason:", reason)
+    clients.remove(sid)
+
+
+async def send_transcript(text: str):
+    return sio.emit("transcript", text)
+
+
+def start_websocket():
+    app = web.Application()
+    sio.attach(app)
+    web.run_app(app, port=25565)
+
+
+# sio.emit("transcript", {"data": "foobar"})
 
 if __name__ == "__main__":
+    start_websocket()
     print("Starting echo server...")
-    asyncio.run(main=main())
