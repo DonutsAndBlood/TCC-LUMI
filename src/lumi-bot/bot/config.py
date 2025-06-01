ba4b7730d99f14a23ffe3ffd683e9b14f884160d
Henrique Klayton <henrique-klayton@users.noreply.github.com>
2025-06-01 13:42:38 -0300

    Adicionar novas variáveis env e criar exemplo

--- src/lumi-bot/bot/config.py
+++ src/lumi-bot/bot/config.py
@@ -4,3 +4,6 @@ import os
 load_dotenv()
 
 DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
+API_URL = os.getenv("API_URL", "localhost")
+API_PORT = os.getenv("API_PORT", "25565")
+ENV = os.getenv("ENV", "production")
