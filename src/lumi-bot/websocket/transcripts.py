3f11185cc35f7d301fa9451c2847446c2edba706
Henrique Klayton <henrique-klayton@users.noreply.github.com>
2025-06-09 03:43:30 -0300

    Usar queue para enviar transcrições para outra thread

--- src/lumi-bot/websocket/transcripts.py
+++ src/lumi-bot/websocket/transcripts.py
@@ -6,6 +6,18 @@ from .server import sio
 queue: Queue[str] = Queue()
 
 
-async def send_transcript(text: str) -> None:
+async def send(text: str) -> None:
     """Send the audio transcript to all connected clients."""
     await sio.emit("transcript", text)
+
+
+async def process_transcripts():
+    while True:
+        if queue.empty():
+            await asyncio.sleep(1)
+            continue
+
+        text = queue.get_nowait()
+
+        # Now you can call the original send_transcript function safely in this event loop
+        await send(text)
